8921a1fe7334dcc9003da7e3de516381b1142fe3
andrey volk <andrey.volk.1999@gmail.com>
2024-10-16 10:05:32 +0300

    Initial commit

---
+++ src/main/java/sky/pro/kurs_project_2/KursProject2Application.java
@@ -0,0 +1,13 @@
+package sky.pro.kurs_project_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/kurs_project_2/KursProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.kurs_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
